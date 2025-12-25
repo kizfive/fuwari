9acce3d8574ebc53f695b972f08d993f5f61e8c4
unknown <3424295311@qq.com>
2025-12-26 01:29:06 +0800

    vault backup: 2025-12-26 01:29:06

--- src/config.ts
+++ src/config.ts
@@ -53,27 +53,37 @@ export const navBarConfig: NavBarConfig = {
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+	name: "Notess",
+	bio: "What can i say?",
 	links: [
-		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
+		// {
+		// 	name: "Twitter",
+		// 	icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
+		// 	// You will need to install the corresponding icon set if it's not already included
+		// 	// `pnpm add @iconify-json/<icon-set-name>`
+		// 	url: "https://twitter.com",
+		// },
 		{
 			name: "Steam",
 			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			url: "https://steamcommunity.com/id/kizfive/",
 		},
 		{
-			name: "GitHub",
-			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			name:"Openlist",
+			icon:"ic:sharp-cloud-circle",
+			url:"http://8.138.20.164:5244/",
 		},
+		{
+			name:"贪吃蛇",
+			icon:"fluent-emoji-high-contrast:beaming-face-with-smiling-eyes",
+			url:"http://8.138.20.164:114/"
+		}
+		// {
+		// 	name: "GitHub",
+		// 	icon: "fa6-brands:github",
+		// 	url: "https://github.com/saicaca/fuwari",
+		// },
 	],
 };
 
